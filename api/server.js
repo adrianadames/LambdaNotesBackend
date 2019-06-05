e41046d2fe21656c15b4d360e89c409d08ba0c61
adrian <adrianadames@gmail.com>
2019-06-05 19:42:16 -0400

    Made API endpoints for getting, posting, and deleting user.

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+const usersRouter = require('./users/usersRouter');
+
+server.use('/api/users', usersRouter);
 
 module.exports = server;
---
+++ api/users/usersRouter.js
@@ -0,0 +1,44 @@
+const db = require('../../data/dbConfig.js');
+const router = require('express').Router();
+
+// api/users/
+
+router.get('/', (req,res) => {
+    db('users')
+        .select()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+router.post('/', (req,res) => {
+    const user = req.body;
+    db.insert(user)
+        .into('users')
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+router.delete('/:id', (req,res) => {
+    const id = req.params.id;
+
+    db('users')
+        .where({id})
+        .del()
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+
+module.exports = router;
