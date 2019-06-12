80a3e82e0225f8d114dfcbf2632b0fc50a046834
adrian <adrianadames@gmail.com>
2019-06-12 18:21:28 -0400

    Made changes so that endpoints return data appropriately formatted to front-end client.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.send('Hello World!');
